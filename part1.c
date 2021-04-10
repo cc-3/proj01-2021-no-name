af271ffa97a236f15d1e5c27c5afb95aa29d3bcd
Axel Olivares <axel.olivares@galileo.edu>
2021-04-09 20:51:55 -0600

    rtype listo

--- part1.c
+++ part1.c
@@ -1,8 +1,7 @@
-#include <stdio.h> // for stderr
+#include <stdio.h>  // for stderr
 #include <stdlib.h> // for exit()
 #include "types.h"
 #include "utils.h"
-
 
 // forward declarations
 void print_rtype(char *, Instruction);
@@ -21,157 +20,257 @@ void write_jalr(Instruction);
 void write_jal(Instruction);
 void write_ecall(Instruction);
 
-
-void decode_instruction(Instruction instruction) {
+void decode_instruction(Instruction instruction)
+{
   /* YOUR CODE HERE: COMPLETE THE SWITCH STATEMENTS */
   unsigned int opcode = instruction.opcode;
-  switch(opcode) { // What do we switch on?
+  switch (opcode)
+  { // What do we switch on?
     /* YOUR CODE HERE */
 
-    case 51:
-      write_rtype(instruction);
+  case 51:
+    write_rtype(instruction);
     break;
 
-    case 19:
-      write_itype_except_load(instruction);
+  case 19:
+    write_itype_except_load(instruction);
     break;
 
-    case 3:
-      write_load(instruction);
+  case 3:
+    write_load(instruction);
     break;
 
-    case 103:
-      write_jalr(instruction);
+  case 103:
+    write_jalr(instruction);
     break;
 
-    case 115:
-      write_ecall(instruction);
+  case 115:
+    write_ecall(instruction);
     break;
 
-    case 35:
-      write_store(instruction);
+  case 35:
+    write_store(instruction);
     break;
 
-    case 99:
-      write_branch(instruction);
+  case 99:
+    write_branch(instruction);
     break;
 
-    case 55:
-      write_lui(instruction);
+  case 55:
+    write_lui(instruction);
     break;
 
-    case 111:
-      write_jal(instruction);
+  case 111:
+    write_jal(instruction);
     break;
 
-    case 0X17:
-      write_auipc(instruction);
+  case 0X17:
+    write_auipc(instruction);
     break;
 
-    default: // undefined opcode
-      handle_invalid_instruction(instruction);
-      break;
+  default: // undefined opcode
+    handle_invalid_instruction(instruction);
+    break;
   }
 }
 
+void write_rtype(Instruction instruction)
+{
+  switch (instruction.rtype.funct3)
+  { // What do we switch on?
+  /* YOUR CODE HERE */
+  case 0:
+    switch (instruction.rtype.funct7)
+    {
+    case 0:
+      print_rtype("add", instruction);
+      break;
 
-void write_rtype(Instruction instruction) {
-  switch(0) { // What do we switch on?
-    /* YOUR CODE HERE */
+    case 1:
+      print_rtype("mul", instruction);
+      break;
+
+    case 32:
+      print_rtype("sub", instruction);
+      break;
+
     default:
       handle_invalid_instruction(instruction);
       break;
-  }
-}
+    }
+    break;
 
+  case 1:
+    switch (instruction.rtype.funct7)
+    {
+    case 0:
+      print_rtype("sll", instruction);
+      break;
 
-void write_itype_except_load(Instruction instruction) {
-  switch(0) { // What do we switch on?
-    /* YOUR CODE HERE */
+    case 1:
+      print_rtype("mulh", instruction);
+      break;
+
     default:
       handle_invalid_instruction(instruction);
       break;
-  }
-}
+    }
+    break;
 
+  case 2:
+    print_rtype("slt", instruction);
+    break;
 
-void write_load(Instruction instruction) {
-  switch(0) { // What do we switch on?
-    /* YOUR CODE HERE */
+  case 4:
+    switch (instruction.rtype.funct7)
+    {
+    case 0:
+      print_rtype("xor", instruction);
+      break;
+
+    case 1:
+      print_rtype("div", instruction);
+      break;
+
     default:
       handle_invalid_instruction(instruction);
       break;
-  }
-}
+    }
+    break;
 
+  case 5:
+    switch (instruction.rtype.funct7)
+    {
+    case 0:
+      print_rtype("srl", instruction);
+      break;
 
-void write_store(Instruction instruction) {
-  switch(0) { // What do we switch on?
-    /* YOUR CODE HERE */
+    case 32:
+      print_rtype("sra", instruction);
+      break;
+    }
+    break;
+
+  case 6:
+    switch (instruction.rtype.funct7)
+    {
+    case 0:
+      print_rtype("or", instruction);
+      break;
+
+    case 1:
+      print_rtype("rem", instruction);
+      break;
+
     default:
       handle_invalid_instruction(instruction);
       break;
+    }
+    break;
+
+  case 7:
+    print_rtype("and", instruction);
+    break;
+
+  default:
+    handle_invalid_instruction(instruction);
+    break;
   }
 }
 
-
-void write_branch(Instruction instruction) {
-  switch(0) { // What do we switch on?
-    /* YOUR CODE HERE */
-    default:
-      handle_invalid_instruction(instruction);
-      break;
+void write_itype_except_load(Instruction instruction)
+{
+  switch (0)
+  { // What do we switch on?
+  /* YOUR CODE HERE */
+  default:
+    handle_invalid_instruction(instruction);
+    break;
   }
 }
 
+void write_load(Instruction instruction)
+{
+  switch (0)
+  { // What do we switch on?
+  /* YOUR CODE HERE */
+  default:
+    handle_invalid_instruction(instruction);
+    break;
+  }
+}
+
+void write_store(Instruction instruction)
+{
+  switch (0)
+  { // What do we switch on?
+  /* YOUR CODE HERE */
+  default:
+    handle_invalid_instruction(instruction);
+    break;
+  }
+}
+
+void write_branch(Instruction instruction)
+{
+  switch (0)
+  { // What do we switch on?
+  /* YOUR CODE HERE */
+  default:
+    handle_invalid_instruction(instruction);
+    break;
+  }
+}
 
 /* For the writes, probably a good idea to take a look at utils.h */
 
-void write_auipc(Instruction instruction) {
+void write_auipc(Instruction instruction)
+{
   /* YOUR CODE HERE */
 }
 
-
-void write_lui(Instruction instruction) {
+void write_lui(Instruction instruction)
+{
   /* YOUR CODE HERE */
 }
 
-
-void write_jalr(Instruction instruction) {
+void write_jalr(Instruction instruction)
+{
   /* YOUR CODE HERE */
 }
 
-
-void write_jal(Instruction instruction) {
+void write_jal(Instruction instruction)
+{
   /* YOUR CODE HERE */
 }
 
-
-void write_ecall(Instruction instruction) {
+void write_ecall(Instruction instruction)
+{
   /* YOUR CODE HERE */
 }
 
+void print_rtype(char *name, Instruction instruction)
+{
+  /* YOUR CODE HERE */
+  printf(RTYPE_FORMAT, name, instruction.rtype.rd, instruction.rtype.rs1, instruction.rtype.rs2);
+}
 
-void print_rtype(char *name, Instruction instruction) {
+void print_itype_except_load(char *name, Instruction instruction, int imm)
+{
   /* YOUR CODE HERE */
 }
 
-
-void print_itype_except_load(char *name, Instruction instruction, int imm) {
+void print_load(char *name, Instruction instruction)
+{
   /* YOUR CODE HERE */
 }
 
-
-void print_load(char *name, Instruction instruction) {
+void print_store(char *name, Instruction instruction)
+{
   /* YOUR CODE HERE */
 }
 
-
-void print_store(char *name, Instruction instruction) {
-  /* YOUR CODE HERE */
-}
-
-
-void print_branch(char *name, Instruction instruction) {
+void print_branch(char *name, Instruction instruction)
+{
   /* YOUR CODE HERE */
 }
