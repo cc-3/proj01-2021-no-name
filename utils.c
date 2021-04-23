2db620053d1d66fa84aea1944ba66e6660ee186f
Axel Olivares <axel.olivares@galileo.edu>
2021-04-23 00:58:49 -0600

    branch offset version 1

--- utils.c
+++ utils.c
@@ -15,15 +15,39 @@ int bitSigner(unsigned int field, unsigned int size) {
 
 /* Remember that the offsets should return the offset in BYTES */
 
+
 int get_branch_offset(Instruction instruction) {
   /* YOUR CODE HERE */
-  return 0;
+  int offset = 0;
+  int offset2 = 0;
+  int offset3 = 0;
+  int offset_final = 0;
+
+  unsigned int digitos_imm7 = instruction.sbtype.imm7 << 5;
+  offset = 0b11111100000 & digitos_imm7;
+  offset_final = offset_final | offset;
+
+  unsigned int digitos_imm5 = 0b11110 & instruction.sbtype.imm5;
+  offset_final = digitos_imm5 | offset;
+
+  unsigned int digitos_imm7_2 = instruction.sbtype.imm7 << 6;
+  offset2 = 0b1000000000000 & digitos_imm7_2;
+  offset_final = offset_final | offset2;
+  
+  unsigned int digitos_imm5_2 = instruction.sbtype.imm5 << 11;
+  int digitos_imm5_3 = 0b100000000000 & digitos_imm5_2;
+  offset_final = offset_final | digitos_imm5_3;
+
+
+  return bitSigner(offset_final, 13);
 }
+
 
 
 int get_jump_offset(Instruction instruction) {
   /* YOUR CODE HERE */
   return 0;
+
 }
 
 
@@ -31,8 +55,6 @@ int get_store_offset(Instruction instruction) {
   /* YOUR CODE HERE */
   return 0;
 }
-
-
 void handle_invalid_instruction(Instruction instruction) {
   printf("Invalid Instruction: 0x%08x\n", instruction.bits);
 }
